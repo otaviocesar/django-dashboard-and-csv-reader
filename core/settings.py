b11c4c55e8dc5e9f7bec85d93e5d7f1c92ad886c
Otavio Cesar Costa Santos <o.caesar@hotmail.com>
2020-10-01 11:13:10 -0300

    add ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -20,9 +20,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-# load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-
+# load production server from .env 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://sistema-eat.herokuapp.com', config('SERVER', default='127.0.0.1')]
 # Application definition
 
 INSTALLED_APPS = [
